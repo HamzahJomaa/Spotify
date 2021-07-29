5ff5af603adbcbdd55e2154b01d539e117fed404
Hamzah Jomaa <hamzah.m.jomaa@gmail.com>
2021-07-29 17:41:28 +0300

    FINAL29JULY

--- src/Layouts/login.js
+++ src/Layouts/login.js
@@ -2,12 +2,7 @@ import React from 'react';
 import {Button} from '@material-ui/core'
 
 
-function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays*60*60*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
+
 function getCookie(cname) {
   var name = cname + "=";
   var decodedCookie = decodeURIComponent(document.cookie);
